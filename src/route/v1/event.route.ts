e7f16e2dc07dbc2e43c2637d20827915a4b537e0
Diego Scarpati <diegoscarpati13@gmail.com>
2024-07-03 11:11:25 +1000

    Component and routes yaml for OpenAPI

--- src/route/v1/event.route.ts
+++ src/route/v1/event.route.ts
@@ -3,9 +3,212 @@ import * as eventController from "../../controller/event.controller"
 
 const eventRouter: Router = express.Router()
 
+/**
+ * @swagger
+ * /event/getAll:
+ * get:
+ *   tags: 
+ *     - Event
+ *   summary: Get all events
+ *   description: Get all events
+ *   responses:
+ *     '200':
+ *       description: A list of events
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Event'
+ *     '500':
+ *       description: Internal server error
+ */
 eventRouter.get("/getAll", eventController.getAllEvents)
+
+/**
+ * @swagger
+ * /event/getAllByAddress:
+ *   get:
+ *     tags: 
+ *       - Event
+ *     summary: Get all events by issuer address
+ *     description: Get all events by issuer address
+ *     parameters:
+ *       - in: query
+ *         name: address
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Issuer address
+ *     responses:
+ *       '200':
+ *         description: A list of events
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Event'
+ *       '500':
+ *         description: Internal server error
+ */
 eventRouter.get("/getAllByAddress", eventController.getAllEventsByIssuersAddress)
+
+/**
+ * @swagger
+ * /event/getByPK:
+ * get:
+ *   tags: 
+ *     - Event
+ *   summary: Get event by primary key
+ *   description: Get event by primary key
+ *   parameters:
+ *     - in: query
+ *       name: eventUuid
+ *       required: true
+ *       schema:
+ *         type: string
+ *         format: uuid
+ *       description: Event primary key
+ *   responses:
+ *     '200':
+ *       description: An event
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Event'
+ *     '404':
+ *       description: Event not found
+ *     '500':
+ *       description: Internal server error
+ */
 eventRouter.get("/getByPK", eventController.getEventByPK)
+
+/**
+ * @swagger
+ * /event/createEvent:
+ * post:
+ *   tags: 
+ *     - Event
+ *   summary: Create new event
+ *   description: Create new event
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             title:
+ *               type: string
+ *               example: "Event title"
+ *             description:
+ *               type: string
+ *               example: "Event description"
+ *             city:
+ *               type: string
+ *               example: "Buenos Aires"
+ *             country:
+ *               type: string
+ *               example: "Argentina"
+ *             startDate:
+ *               type: string
+ *               format: date
+ *               example: "2022-01-01"
+ *             endDate:
+ *               type: string
+ *               format: date
+ *               example: "2022-01-02"
+ *             expiryDate:
+ *               type: string
+ *               format: date
+ *               example: "2022-01-03"
+ *             year:
+ *               type: integer
+ *               example: 2022
+ *             eventUrl:
+ *               type: string
+ *               example: "https: *www.example.com"
+ *             virtualEvent:
+ *               type: boolean
+ *               example: true
+ *             image:
+ *               type: string
+ *               example: "https: *www.example.com/image.jpg"
+ *             secretCode:
+ *               type: string
+ *               example: "secret"
+ *             eventTemplateId:
+ *               type: string
+ *               example: "template"
+ *             email:
+ *               type: string
+ *               format: email
+ *               example: mail@example.com
+ *             requestedCodes:
+ *               type: integer
+ *               example: 100
+ *             privateEvent:
+ *               type: boolean
+ *               example: false
+ *             purpose:
+ *               type: string
+ *               example: "Event purpose"
+ *             platform:
+ *               type: string
+ *               example: "Event platform"
+ *             eventType:
+ *               type: string
+ *               enum:
+ *                 - "Virtual"
+ *                 - "In-Person"
+ *                 - "Both"
+ *               example: "Virtual"
+ *             amountOfAttendees:
+ *               type: integer
+ *               example: 100
+ *             poapType:
+ *               type: string
+ *               enum:
+ *                 - "Poap"
+ *                 - "Soulbound"
+ *                 - "Consensual"
+ *               example: "Poap"
+ *             poapsToBeMinted:
+ *               type: integer
+ *               example: 100
+ *             mintedPoaps:
+ *               type: integer
+ *               example: 50
+ *             idInContract:
+ *               type: integer
+ *               example: 1
+ *           required:
+ *             - title
+ *             - description
+ *             - startDate
+ *             - endDate
+ *             - virtualEvent
+ *             - image
+ *             - email
+ *             - requestedCodes
+ *             - privateEvent
+ *             - platform
+ *             - poapType
+ *             - poapsToBeMinted
+ *             - mintedPoaps
+ *             - idInContract
+ *   responses:
+ *     '201':
+ *       description: Event created
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Event'
+ *     '400':
+ *       description: Invalid request
+ *     '500':
+ *       description: Internal server error
+*/
 eventRouter.post("/createEvent", eventController.createEvent)
 
 
--- src/route/v1/owner.route.ts
+++ src/route/v1/owner.route.ts
@@ -3,11 +3,193 @@ import * as ownerController from "../../controller/owner.controller"
 
 const ownerRouter: Router = express.Router()
 
+/**
+ * @swagger
+ * /owner/getAll:
+ * get:
+ *   tags: 
+ *     - Owner
+ *   summary: Get all owners
+ *   description: Get all owners
+ *   responses:
+ *     '200':
+ *       description: A list of owners
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Owner'
+ */
 ownerRouter.get("/getAll", ownerController.getAllOwners)
+
+/**
+ * @swagger
+ * /owner/getOwnerByAddress:
+ * get:
+ *   tags: 
+ *     - Owner
+ *   summary: Get owner by address
+ *   description: Get owner by address
+ *   parameters:
+ *     - in: query
+ *       name: address
+ *       required: true
+ *       schema:
+ *         type: string
+ *       description: Owner address
+ *   responses:
+ *     '200':
+ *       description: An owner
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Owner'
+ *     '404':
+ *       description: Owner not found
+ */
 ownerRouter.get("/getOwnerByAddress", ownerController.getOwnerByAddress)
+
+/**
+ * @swagger
+ * /owner/getOwnerByEmail:
+ * get:
+ *   tags: 
+ *     - Owner
+ *   summary: Get owner by email
+ *   description: Get owner by email
+ *   parameters:
+ *     - in: query
+ *       name: email
+ *       required: true
+ *       schema:
+ *         type: string
+ *       description: Owner email
+ *   responses:
+ *     '200':
+ *       description: An owner
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Owner'
+ *     '404':
+ *       description: Owner not found
+ */
 ownerRouter.get("/getOwnerByEmail", ownerController.getOwnerByEmail)
+
+/**
+ * @swagger
+ * /owner/getOwnerByPK:
+ * get:
+ *   tags: 
+ *     - Owner
+ *   summary: Get owner by primary key
+ *   description: Get owner by primary key
+ *   parameters:
+ *     - in: query
+ *       name: ownerUuid
+ *       required: true
+ *       schema:
+ *         type: string
+ *         format: uuid
+ *       description: Owner primary key
+ *   responses:
+ *     '200':
+ *       description: An owner
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Owner'
+ *     '404':
+ *       description: Owner not found
+ */
 ownerRouter.get("/getOwnerByPK", ownerController.getOwnerByPK)
+
+/**
+ * @swagger
+ * /owner/createOwner:
+ *     post:
+ *       tags: 
+ *         - Owner
+ *       summary: Create new owner
+ *       description: Create new owner
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 address:
+ *                   type: string
+ *                   format: address
+ *                   example: "0xdD2FD4581271e330360230F9337D5c0430Bf44C0"
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   example: mail@example.com
+ *               required:
+ *                 - address
+ *                 - email
+ *       responses:
+ *         '201':
+ *           description: Owner created
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Owner'
+ *         '400':
+ *           description: Invalid request
+ *         '500':
+ *           description: Internal server error
+ */
 ownerRouter.post("/createOwner", ownerController.createNewOwner)
+
+/**
+ * @swagger
+ * /owner/updateOwner:
+ *     patch:
+ *       tags: 
+ *         - Owner
+ *       summary: Update owner
+ *       description: Update owner
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ownerUuid:
+ *                   type: string
+ *                   format: uuid
+ *                   example: 123e4567-e89b-12d3-a456-426614174000
+ *               required:
+ *                 - ownerUuid
+ *               anyOf:
+ *                 - properties:
+ *                     email:
+ *                       type: string
+ *                       format: email
+ *                       example: mail@example.com
+ *                 - properties:
+ *                     address:
+ *                       type: string
+ *                       format: address
+ *                       example: "0xdD2FD4581271e330360230F9337D5c0430Bf44C0"
+ *       responses:
+ *         '200':
+ *           description: Owner updated successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Owner'
+ *         '400':
+ *           description: Invalid request
+ *         '404':
+ *           description: Owner not found
+ *         '500':
+ *           description: Internal server error
+ */
 ownerRouter.patch("/updateOwner", ownerController.updateOwner)
 
 
